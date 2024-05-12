f88ef980e3f1f1a504787a6340e9b73e9861480f
HGX Server <phananhduc138@gmail.com>
2024-05-12 17:21:41 +0900

    full label filling and balancing

--- src/data/components/analyzer.py
+++ src/data/components/analyzer.py
@@ -21,9 +21,9 @@ class Analyzer:
                 manifest_path, 
                 cache_dir = "cache",
                 data_dir = "/data/hpc/"):
-        with open(manifest_path, "r") as file:
-            self.dataset = json.load(file)
         
+        self.dataset = None
+
         self.train_full_dist = dict()
         self.train_hard_dist = dict()
         self.val_dist = dict()
@@ -34,23 +34,29 @@ class Analyzer:
         self.val = dict()
 
         # cache new state of dataset in case algorithm gone wrong :) 
-        self.new_train = dict()
-        self.new_train_hard = dict()
-        self.cache = dict()
+        self.new_train = None
+        self.new_train_hard = None
+        self.cache = None
+        self.cache_dist = None
 
         # path for cp command
         self.cache_dir = cache_dir
         self.data_dir = data_dir
+        self.total = 0
+        self.prepare(manifest_path)
 
-    def prepare(self):
+    def prepare(self, manifest_path):
         """ Prepare sample sets for processing
         """
-        train_dataset = self.dataset['train']
+        with open(manifest_path, "r") as file:
+            dataset = json.load(file)
+            self.dataset = dataset
+        train_dataset = dataset['train']
         keys = list(train_dataset.keys())
         train_full_dist = [len(data) for data in train_dataset.values()]
         self.train_full_dist = dict(zip(keys, train_full_dist))
         
-        val_dataset = self.dataset['val']
+        val_dataset = dataset['val']
         val_keys = list(val_dataset.keys())
         self.val_dist = dict(zip(keys, np.zeros(len(keys), dtype=int).tolist()))
         
@@ -73,26 +79,45 @@ class Analyzer:
             self.val_dist[key] += len(val_dataset[key])
         
         self.train = train_dataset
-        self.val = self.dataset['val']
+        self.val = dataset['val']
+        
+        # load cache, cache included means new state :D
+        if 'cache' in dataset.keys():
+            self.cache = dataset['cache']
+            self.cache_dist = dict()
+            for key in keys:
+                    self.cache_dist[key] = len(self.cache[key]) if key in self.cache.keys() else 0
+        
+        # To manage dataset description.
+        if self.total == 0:
+            self.total = np.sum(list(self.train_full_dist.values())) + np.sum(list(self.val_dist.values()))
+            if isinstance(self.cache, dict):
+                self.total += np.sum([len(label) for label in self.cache.values()])
+            print("Total samples:", self.total)
+        else: 
+            ref = np.sum(list(self.train_full_dist.values())) + np.sum(list(self.val_dist.values()))
+            if isinstance(self.cache, dict):
+                ref += np.sum([len(label) for label in self.cache.values()])
+            print("Total samples differences:", ref - self.total)
 
     def valid_fill_label(self, alpha=0.8):
         """ This function analyze the missing labels, also label unbalance between train and val
             Thus, propose a new manifest that make sure every labels have its sample in validation set"""
         # backward folder path
-
-        t_dist = np.array(list(self.train_full_dist.values()))
         v_dist = np.array(list(self.val_dist.values()))
-
+        if self.cache_dist:
+            v_dist += np.array(list(self.cache_dist.values()))
+        
         v_mean = np.mean(v_dist[v_dist > 0])
         print("Mean:", v_mean)
         
         v_std = np.sqrt(np.var(v_dist[v_dist != 0]))
-
+        
 
         # get missing label indexes
         v_missing = (v_dist < 1).astype(int)
         v_deviation = v_dist - v_mean
-        v_upsample = v_deviation - v_std
+        v_upsample = v_deviation
 
         # Exclude all missing samples, suffient sample set.
         v_upsample[(np.abs(v_deviation) <= v_std / alpha) | 
@@ -110,23 +135,23 @@ class Analyzer:
         print("Valid standard deviation", v_std)
         print("Missing labels", np.sum(v_missing))
 
-        return v_dist, dict(zip(keys, v_upsample)), dict(zip(keys, v_missing))
+        return v_mean, v_std, v_dist, dict(zip(keys, v_upsample)), dict(zip(keys, v_missing))
         
-    def query_train(self, v_missing, maximum_exploit=0.4):
+    def query_train(self, query, maximum_exploit=0.4):
         """ Retrieve training examples as valid label filling...
             
             Args:
-                v_missing: missing labels with required sample quantities
+                query: samples query with required sample quantities from training set
                 maximum_exploit: Maximum percentage of samples can be taken from training hard examples
         """
         # Being sures that all dictionary share the same key sets,
         # Else this algorithm will break
 
-        assert list(v_missing.keys()) == list(self.train_hard_dist.keys())
+        assert list(query.keys()) == list(self.train_hard_dist.keys())
         train_h_dist = np.array(list(self.train_hard_dist.values()))
         train_dist = np.array(list(self.train_full_dist.values())) - train_h_dist
         
-        v_req = np.array(list(v_missing.values()))
+        v_req = np.array(list(query.values()))
         
         train_h_avail = np.floor(train_h_dist * maximum_exploit).astype(int)
         
@@ -138,11 +163,11 @@ class Analyzer:
         # in other case, take from ordinary samples - Thuong samples
         v_n_req = np.min(np.stack([v_req - v_h_req, train_avail], axis=1), axis=1)
         
-        return dict(zip(list(v_missing.keys()), v_req.tolist())), dict(zip(list(v_missing.keys()), v_h_req.tolist())), dict(zip(list(v_missing.keys()), v_n_req.tolist()))
+        return  dict(zip(list(query.keys()), v_req.tolist())), dict(zip(list(query.keys()), v_h_req.tolist())), dict(zip(list(query.keys()), v_n_req.tolist()))
 
 
     def extract(self, req, h_req, n_req, maximum_exploit=0.4, export=None, refresh=False):
-        """ This function diversify the validation set by m
+        """ This function diversify the validation set by taking samples from training set. 
         """
         if refresh is True:
             delete_contents_of_folder(self.data_dir + "/cache")
@@ -153,7 +178,10 @@ class Analyzer:
         for key in list(self.new_train.keys()):
             if req[key] == 0:
                 continue
-            self.cache[key] = []
+
+            # make sure that cache also have same keys with train set    
+            if key not in self.cache.keys():
+                self.cache[key] = []
 
             for i in range(h_req[key]):
                 index = np.random.choice(range(len(self.new_train_hard[key])))
@@ -170,7 +198,10 @@ class Analyzer:
                 shutil.copy(self.data_dir + "/train/" + data, self.data_dir + "/cache/" + fname)
             
             # in case that all request violated the exploit limit
-            if len(self.cache[key]) == 0:
+            # only for missing labels  and demanding samples
+            if len(self.cache[key]) == 0 and refresh is True and req[key] != 0:
+                print("Insufficient dataset, undergo copying samples label {}".format(key))
+
                 if self.train_full_dist[key] <= np.ceil(1 / maximum_exploit):
                     index =  np.random.choice(range(self.train_full_dist[key]))
                     data = self.dataset['train'][key][index]
@@ -193,6 +224,18 @@ class Analyzer:
             with open(export, "w") as f:
                 json.dump(output, f, indent=4)
                 f.close()
-                
+
+            # Reload dataset
+            self.prepare(export)
+
+    def train_loss_weight(self):
+        assert self.val_dist.keys() != len(self.train_full_dist.keys()), "Validation set label vanished or overtaken or misordered from training set !" 
         
+        # v_d = np.array([len(value) for value in self.val.values()]) + np.array([len(value) for value in self.cache.values()])
+        t_d = np.array(list(self.train_full_dist.values()))
+        ratio = np.sqrt(np.mean(t_d) / t_d)
+
+        return ratio
+        
+
     # def query_train(self, )
